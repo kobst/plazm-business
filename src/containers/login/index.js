e9105fd36d75d9919809709936aa86daacd72a6e
ritwik.agrawal <ritwik.agrawal@vtnetzwelt.com>
2020-06-15 20:23:51 +0530

    Added Login and Registarion Page

---
+++ src/containers/login/index.js
@@ -0,0 +1,112 @@
+import React, {useState, useEffect} from 'react'
+import './login.css'
+import { Auth } from 'aws-amplify';
+
+const Login = () => {
+    const [user,setUser]= useState()
+    const [password,setPassword]= useState()
+    const [signedIn,setSignedIn]= useState(false)
+    const [error,setError] = useState(false)
+    useEffect(() => {
+        let updateUser = async authState => {
+          try {
+             await Auth.currentAuthenticatedUser()
+            setSignedIn(true)
+            
+          } catch {
+              console.log('okkay')
+           
+          }
+        }
+        updateUser()
+      }, []);
+
+    const signIn = () => {
+        Auth.signIn({
+            username: user,
+            password: password
+        })
+        .then(() =>{  setSignedIn(true) 
+            console.log('successfully signed in')})
+        .catch((err) => setError(true))
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+         signIn()
+         setUser('')
+         setPassword('')
+         e.target.reset()
+}
+
+   const handleChange = e => {
+        if (e.target.id === 'username') {
+          setUser(e.target.value)
+          
+        } else if (e.target.id === 'password') {
+          setPassword(e.target.value)
+        }
+    }
+
+
+  
+    if (signedIn) {
+        return (
+            <div>
+                <h1>You have signed in!</h1>
+                <button type="submit" onClick = {() => (
+                    // eslint-disable-next-line no-sequences
+                    Auth.signOut(),
+                     window.location.reload())} className="btn btn-primary">Logout</button>
+            </div>
+        );
+    } 
+    else{
+
+    return(
+    <div className="login-wrapper">
+        <div className="container-fluid">
+        <div className="row">
+        <div className="col-md-6 cover-image"> </div>
+			<div className="col-md-6">
+                <div className="login-form-wrapper">
+					<div className="login-form-header">
+						<h1>Howdy! Welcome Back</h1>
+						<p> login to start working on your business profile page </p>
+					</div>
+                    <div className="login-form-nested-wrapper login-fields-spacing"> 
+                    <form onSubmit={ (e) => handleSubmit(e) }>
+							<div className="form-group">
+                            <input type="email" id='username' onChange={(e) => handleChange(e)} className="form-control" placeholder="Email address"/>
+							</div>
+                            <div className="form-group">
+								<input type="password" id='password' onChange={(e) => handleChange(e)} className="form-control" placeholder="Password" />
+							</div>
+							<div className="form-group remember-checkbox">
+								<input type="checkbox" id="rememberMe" />
+								<label htmlFor="rememberMe">Remember me</label>
+							</div>	
+							<button type="submit" className="btn btn-primary">Login</button>
+                            {error ?<div class="form-group"><br /><h6> Invalid Username or Password</h6></div>: null}
+                            <div className="login-links-wrapper login-links-extra-links">
+								<a href="http://localhost:3000/register" className="link-btn"> Don't have an account? <strong>Signup</strong></a>
+								<a href="http://localhost:3000/forgot-password" className="link-btn"> Forgot Password </a>
+							</div>
+							
+						</form>
+                    
+                    
+                    </div>
+
+            </div>
+        
+
+            </div>
+        </div>
+    </div>
+    </div>
+    )
+    }
+}
+
+export default Login
---
+++ src/containers/register/index.js
@@ -0,0 +1,173 @@
+import React, {useState} from 'react'
+import './register.css'
+import { Auth } from 'aws-amplify';
+import history from '../../utils/history'
+
+const Register = () => {
+    const [username,setUser]= useState()
+    const [password,setPassword]= useState()
+    const [verified,setVerified]= useState(false)
+    const [email,setEmail]= useState()
+    const [confirmationCode,setconfirmationCode]= useState()
+    const [phone_number,setPhoneNumber]= useState()
+    const [err,setError] = useState(false)
+    const [message,setMessage] = useState()
+    const [codeError,setCodeError] = useState(false)
+
+    const signUp = () => {
+        Auth.signUp({
+            username: email,
+            password: password,
+            attributes: {
+                email: email,
+                phone_number: phone_number, 
+                name: username,
+            }
+        })
+        .then(() => {
+            setVerified(true)
+        })
+        .catch((err) => setMessage(err.message) , setError(true) )
+    }
+  
+    const confirmSignUp = () => {
+        Auth.confirmSignUp(email, confirmationCode)
+        .then(() => (
+           // eslint-disable-next-line no-sequences
+           history.push('/login'),
+           window.location.reload()
+        ))
+        .catch((err) => setCodeError(true))
+    }
+  
+   const handleSubmit = (e) => {
+        e.preventDefault();
+  
+        if (verified) {
+          confirmSignUp();
+          setconfirmationCode('')
+          setUser('')
+        } else {
+          signUp();
+          setPassword()
+          setPhoneNumber()
+        }
+        e.target.reset();
+    }
+  
+    const handleChange = (e) => {
+        setError(false)
+        setCodeError(false)
+        if (e.target.id === 'username') {
+            setUser(e.target.value)
+        } else if (e.target.id === 'password') {
+          setPassword(e.target.value)
+        } else if (e.target.id === 'phone_number') {
+          setPhoneNumber(e.target.value)
+        } else if (e.target.id === 'email') {
+          setEmail(e.target.value)
+        } else if (e.target.id === 'confirmationCode') {
+          setconfirmationCode(e.target.value)
+        }
+    }
+    return(
+        <div className="login-wrapper">
+        <div className="container-fluid">
+            <div className="row">			
+                <div className="col-md-6 cover-image"> </div>
+                <div className="col-md-6">
+                    
+                    <div className="login-form-wrapper">
+                    
+                        <div className="login-form-header">
+                            <h1>Register to Get Started</h1>
+                            <p> Start working on your business profile page </p>
+                        </div>
+                       
+                        <div className="login-form-nested-wrapper">
+                            { verified ? ( <form onSubmit={ (e)=> handleSubmit(e) }>
+                            <div className="row">
+                                    <div className="col-md-6">
+                                    <p> Enter the Confirmation code sent to your Registered Email</p>
+                                        <div className="form-group">
+                                          <input id='confirmationCode' type='text' onChange={ (e) => handleChange(e)} placeholder="Confirmation Code"/>
+                                        </div>
+                                        <button type="submit" className="btn btn-primary">Confirm Sign up</button>
+                                        {codeError ?<div class="form-group"><br /><h6>Confirmation code does not match</h6></div>: null}
+                                     </div>
+                          </div>
+                        </form> ) :
+                            <form onSubmit={ (e)=> handleSubmit(e) }>
+                                <div className="row">
+                                    <div className="col-md-6">
+                                        <div className="form-group">
+                                            <input type="text" id="username" onChange={ (e) => handleChange(e) } className="form-control" placeholder="First Name"/>
+                                        </div>									
+                                    </div>
+                                    <div className="col-md-6">
+                                        <div className="form-group">
+                                            <input id="last_name" onChange={ (e) => handleChange(e)} type="text" className="form-control" placeholder="Last Name"/>
+                                        </div>									
+                                    </div>								
+                                </div>
+                                <div className="row">
+                                    <div className="col-md-12">
+                                        <div className="form-group">
+                                            <input id='phone_number' onChange={ (e) => handleChange(e) } className="form-control" placeholder="Phone Number"/>
+                                        </div>									
+                                    </div>
+                                </div>		
+    
+            
+                                
+                                <div className="row">
+                                    <div className="col-md-12">
+                                        <div className="form-group">
+                                            <input type="email" id='email' onChange={ (e) => handleChange(e) } className="form-control" placeholder="Email address"/>
+                                        </div>									
+                                    </div>
+                                </div>	
+    
+    
+                                <div className="row">
+                                    <div className="col-md-12">
+                                        <div className="form-group">
+                                            <input type="password" id="password" onChange={ (e) => handleChange(e) } className="form-control" placeholder="Password"/>
+                                        </div>									
+                                    </div>
+                                </div>
+    
+    
+                          
+                                <div className="find-your-business-wrapper">
+                                    <h2> Find Your Business</h2>
+                                    <p>By clicking register, I represent I have read, understand, and agree to the Postmates Privacy Policy and Terms of Service. This site is protected bt reCAPTCHA and google Privacy Policy and Terms of Service apply.</p>
+                                </div>
+                            
+                                
+    
+                                <button type="submit" className="btn btn-primary">Register</button>
+                                 {err ?<div class="form-group"><br /><h6>{message}</h6></div>: null}
+                                <div className="login-links-wrapper login-links-extra-links">
+                                    <a href="login.html" className="link-btn">Already have an account? <strong>Log In</strong></a>
+                                </div>
+                          							
+    
+                                
+                            </form>
+}
+                        </div>
+                  
+    
+                    </div>
+              
+    
+                    
+                </div>			
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export default Register
