33addfa5a3cb05000e27540af8b6c7e5607f4414
ritwik.agrawal <ritwik.agrawal@vtnetzwelt.com>
2020-09-09 10:21:45 +0530

    minor-changes

--- src/component/Login-Register/Form-Components/Login-Form.js
+++ src/component/Login-Register/Form-Components/Login-Form.js
@@ -56,13 +56,13 @@ return(
 							  <FormGroup>
                   <Label name="Enter your login ID" />
                       <Input type="text" id='username' onChange={(e) => handleChange(e)} 
-                        error={userError}  placeholder="Email address"/>
+                        error={userError}/>
                         {error && (message ==='User does not exist.'||message ==='Invalid Email') ?<ErrorMessage>{message}</ErrorMessage>: null}
 							  </FormGroup>
                 <FormGroup>
                     <Label name="Password" />
                         <Input type="password" id='password' onChange={(e) => handleChange(e)}
-                          error={passwordError} placeholder="Password" />
+                          error={passwordError} />
                           {error && message !=='User does not exist.' && message !=='Invalid Email' ?<ErrorMessage>{message}</ErrorMessage>: null}
 							  </FormGroup>	
                 <Button type="submit" className="btn btn-primary">{loader && !message? <ValueLoader /> : renderMessage.Log}</Button>
