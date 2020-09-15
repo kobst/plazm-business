89afe9934b62c2222a0c319e6f5aa4104d04fb39
ritwik.agrawal <ritwik.agrawal@vtnetzwelt.com>
2020-09-15 19:15:28 +0530

    minor-changes

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2a9233315c8d863a194249358438ed96.js"
+  "/precache-manifest.51f4948ffc1035959098392c40b29832.js"
 );
 
 self.addEventListener('message', (event) => {
