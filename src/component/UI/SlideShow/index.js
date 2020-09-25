d5c645df6fa4f870678b92b730a3a7e5baa192e2
Pankaj <pankaj.sehgal@vtnetzwelt.com>
2020-09-25 14:26:19 +0530

    Slider-UI-Fix

--- src/component/UI/SlideShow/index.js
+++ src/component/UI/SlideShow/index.js
@@ -4,15 +4,28 @@ import "react-alice-carousel/lib/alice-carousel.css";
 
 const SlideShow = ({image,currentIndex})=> {
   const  responsive = {
-        0: { items: 1 },
-        2000: { items: 2 },
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      900: {
+        items: 1
+      },
+      1200: {
+        items: 1
       }
+       
+    }
+
+    
 
 
     return (
-       <AliceCarousel slideToIndex={currentIndex} responsive={responsive} autoPlay autoPlayInterval="2000">
+       <AliceCarousel slideToIndex={currentIndex} responsive={responsive} >
            {image ? image.map(v=> 
-        <img src={v} className="sliderimg" alt=""/>
+          <img src={v} className="" alt=""/>
         ):null}
       </AliceCarousel>
     );
