cf3b9942d3708ab046e99a407d48d19baec52323
ritwik.agrawal <ritwik.agrawal@vtnetzwelt.com>
2020-12-30 16:22:37 +0530

    adding production file for staging

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.24501b22d73cc371d976e69bda61d5a3.js"
+  "/precache-manifest.b5d43d139c66302db2151b59de0d390f.js"
 );
 
 self.addEventListener('message', (event) => {
