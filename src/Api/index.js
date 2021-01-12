40a4b91054b3ef23e2df0493079274e954141038
kobst <edwrdhn@gmail.com>
2021-01-12 16:26:43 -0500

    committing minor changes

--- src/Api/index.js
+++ src/Api/index.js
@@ -1,5 +1,5 @@
-export const callPlace = async(userSub) => {
-    const response= await fetch(`${process.env.REACT_APP_API_URL}/api/place-fetch/${userSub}`, {
+export const callPlace = async (userSub) => {
+  const response = await fetch(`${process.env.REACT_APP_API_URL}/api/place-fetch/${userSub}`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
@@ -8,9 +8,9 @@ export const callPlace = async(userSub) => {
   const body = await response.text();
   const val = JSON.parse(body)
   return val
-  }
-  export const fetchPosts = async(id) => {
-    const response= await fetch(`${process.env.REACT_APP_API_URL}/api/posts/${id}`, {
+}
+export const fetchPosts = async (id) => {
+  const response = await fetch(`${process.env.REACT_APP_API_URL}/api/posts/${id}`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
@@ -19,10 +19,10 @@ export const callPlace = async(userSub) => {
   const body = await response.text();
   const val = JSON.parse(body)
   return val
-  }
+}
 
-  export const fetchComments = async(id) => {
-    const response= await fetch(`${process.env.REACT_APP_API_URL}/api/comments/${id}`, {
+export const fetchComments = async (id) => {
+  const response = await fetch(`${process.env.REACT_APP_API_URL}/api/comments/${id}`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
@@ -31,9 +31,9 @@ export const callPlace = async(userSub) => {
   const body = await response.text();
   const val = JSON.parse(body)
   return val
-  }
-  export const fetchEvents = async(id) => {
-    const response= await fetch(`${process.env.REACT_APP_API_URL}/api/events/${id}`, {
+}
+export const fetchEvents = async (id) => {
+  const response = await fetch(`${process.env.REACT_APP_API_URL}/api/events/${id}`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
@@ -42,9 +42,9 @@ export const callPlace = async(userSub) => {
   const body = await response.text();
   const val = JSON.parse(body)
   return val
-  }
-  export const fetchUsers = async(id) => {
-    const response= await fetch(`${process.env.REACT_APP_API_URL}/api/accounts`, {
+}
+export const fetchUsers = async (id) => {
+  const response = await fetch(`${process.env.REACT_APP_API_URL}/api/accounts`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
@@ -53,10 +53,10 @@ export const callPlace = async(userSub) => {
   const body = await response.text();
   const val = JSON.parse(body)
   return val
-  }
+}
 
- export const callApi = async(name) => {
-    const response= await fetch(`${process.env.REACT_APP_API_URL}/api/place/${name}`, {
+export const callApi = async (name) => {
+  const response = await fetch(`${process.env.REACT_APP_API_URL}/api/place/${name}`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
@@ -65,70 +65,70 @@ export const callPlace = async(userSub) => {
   const body = await response.text();
   return JSON.parse(body)
 }
-  
-export const addBusiness = async (userSub,businessInfo) => {
-    const response= await fetch(`${process.env.REACT_APP_API_URL}/api/place`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            
-                status: businessInfo.business_status,
-                userAddress: businessInfo.formatted_address,
-                telephone: businessInfo.international_phone_number,
-                companyName:businessInfo.name,
-                placeId: businessInfo.place_id,
-                mapLink:businessInfo.url,
-                rating: businessInfo.rating,
-                website: businessInfo.website,
-                userSub: userSub,
-                latitude: businessInfo.geometry.location.lat(),
-                longitude: businessInfo.geometry.location.lng(),
-            
-      })
-    });
-      const body = await response.text();
-      return body
+
+export const addBusiness = async (userSub, businessInfo) => {
+  const response = await fetch(`${process.env.REACT_APP_API_URL}/api/place`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+
+      status: businessInfo.business_status,
+      userAddress: businessInfo.formatted_address,
+      telephone: businessInfo.international_phone_number,
+      companyName: businessInfo.name,
+      placeId: businessInfo.place_id,
+      mapLink: businessInfo.url,
+      rating: businessInfo.rating,
+      website: businessInfo.website,
+      userSub: userSub,
+      latitude: businessInfo.geometry.location.lat(),
+      longitude: businessInfo.geometry.location.lng(),
+
+    })
+  });
+  const body = await response.text();
+  return body
 
 }
-export const updateBusiness = async (value,userSub) => {
-    const response= await fetch(`${process.env.REACT_APP_API_URL}/api/place`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({  
-            _id:value._id,
-            status: value.status,
-            userAddress: value.address,
-            telephone: value.telephone,
-            companyName:value.company_name,
-            placeId: value.place_id,
-            mapLink:value.url,
-            type:value.type,
-            rating: value.rating,
-            website: value.web_site,
-            userSub: userSub,
-            latitude: value.latitude,
-            longitude: value.longitude,
-            city:value.city,
-            postalCode:value.postalCode,
-            stateProvince:value.stateProvince,
-            twitter: value.handles.twitter,
-            instagram: value.handles.instagram,
-            facebook: value.handles.facebook,
-            linkedin:value.handles.linkedin,
-            filterTags: value.filter_tags,
-            file:value.default_image_url,
-            price:value.price,
-            genType:value.genType,
-            openingHours:value.hours,
-                
-            
-      })
-    });
-      const body = await response.text();
-      return body
+export const updateBusiness = async (value, userSub) => {
+  const response = await fetch(`${process.env.REACT_APP_API_URL}/api/place`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      _id: value._id,
+      status: value.status,
+      userAddress: value.address,
+      telephone: value.telephone,
+      companyName: value.company_name,
+      placeId: value.place_id,
+      mapLink: value.url,
+      type: value.type,
+      rating: value.rating,
+      website: value.web_site,
+      userSub: userSub,
+      latitude: value.latitude,
+      longitude: value.longitude,
+      city: value.city,
+      postalCode: value.postalCode,
+      stateProvince: value.stateProvince,
+      twitter: value.handles.twitter,
+      instagram: value.handles.instagram,
+      facebook: value.handles.facebook,
+      linkedin: value.handles.linkedin,
+      filterTags: value.filter_tags,
+      file: value.default_image_url,
+      price: value.price,
+      genType: value.genType,
+      openingHours: value.hours,
+
+
+    })
+  });
+  const body = await response.text();
+  return body
 
 }
--- src/containers/Login-Register/Register.js
+++ src/containers/Login-Register/Register.js
@@ -1,287 +1,350 @@
-import React, {useState,useEffect} from 'react'
+import React, {
+  useState,
+  useEffect
+} from 'react'
 import './style.css'
-import { Auth } from 'aws-amplify';
+import {
+  Auth
+} from 'aws-amplify';
 import history from '../../utils/history'
 import Wrapper from '../../component/Login-Register/Wrapper'
 import RegisterForm from '../../component/Login-Register/Form-Components/Register-Form'
-import {getMessage} from '../../config'
+import {
+  getMessage
+} from '../../config'
 import ValueLoader from '../../utils/loader'
-import {callApi,addBusiness, updateBusiness} from '../../Api'
+import {
+  callApi,
+  addBusiness,
+  updateBusiness
+} from '../../Api'
 
-const renderMessage= getMessage()
+const renderMessage = getMessage()
 
 const Register = (props) => {
-    const type = props.match.url
-    const [username,setUser]= useState()
-    const [password,setPassword]= useState()
-    const [verified,setVerified]= useState(false)
-    const [email,setEmail]= useState()
-    const [loc,setLoc]= useState()
-    const [confirmationCode,setconfirmationCode]= useState()
-    const [phone_number,setPhoneNumber]= useState()
-    const [err,setError] = useState(false)
-    const [message,setMessage] = useState()
-    const [businessInfo,setBusinessInfo] = useState()
-    const [name,setName]= useState()
-    const [codeError,setCodeError] = useState(false)
-    const [firstNameError,setFirstNameError] = useState(false)
-    const [firstError,setFirstError] = useState(false)
-    const [emailError,setEmailError] = useState(false)
-    const [phoneError, setPhoneError] = useState(false)
-    const [passwordError,setPasswordError] = useState(false)
-    const [locationError,setLocError]= useState(false)
-    const [business,setbusiness] = useState(false)
-    const [loader,setLoader] = useState(false)
-    const [emptyCode,setEmptyCodeError]= useState(false)
-    const [phoneShort,setPhoneShort]= useState(false)
-    const [phoneLong,setPhoneLong]= useState(false)
-    const [loginValue,setLoginValue]= useState(false)
-    const [disable,setDisable]= useState(false)
-    useEffect(() => {
-        let updateUser = async authState => {
-          try {
-             await Auth.currentAuthenticatedUser()
-             history.push('/dashboard')
-             window.location.reload() 
-            
-          } catch {
-              setLoginValue(true)
-          }
-        }
-        updateUser()
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      }, []);
-    
+  const type = props.match.url
+  const [username, setUser] = useState()
+  const [password, setPassword] = useState()
+  const [verified, setVerified] = useState(false)
+  const [email, setEmail] = useState()
+  const [loc, setLoc] = useState()
+  const [confirmationCode, setconfirmationCode] = useState()
+  const [phone_number, setPhoneNumber] = useState()
+  const [err, setError] = useState(false)
+  const [message, setMessage] = useState()
+  const [businessInfo, setBusinessInfo] = useState()
+  const [name, setName] = useState()
+  const [codeError, setCodeError] = useState(false)
+  const [firstNameError, setFirstNameError] = useState(false)
+  const [firstError, setFirstError] = useState(false)
+  const [emailError, setEmailError] = useState(false)
+  const [phoneError, setPhoneError] = useState(false)
+  const [passwordError, setPasswordError] = useState(false)
+  const [locationError, setLocError] = useState(false)
+  const [business, setbusiness] = useState(false)
+  const [loader, setLoader] = useState(false)
+  const [emptyCode, setEmptyCodeError] = useState(false)
+  const [phoneShort, setPhoneShort] = useState(false)
+  const [phoneLong, setPhoneLong] = useState(false)
+  const [loginValue, setLoginValue] = useState(false)
+  const [disable, setDisable] = useState(false)
+  useEffect(() => {
+    let updateUser = async authState => {
+      try {
+        await Auth.currentAuthenticatedUser()
+        history.push('/dashboard')
+        window.location.reload()
 
-    const signUp = (form) => {
-        Auth.signUp({
-            username: email,
-            password: password,
-            attributes: {
-                email: email,
-                phone_number: phone_number, 
-                name: username,
-            }
-        })
-        .then(async(res) => {
-            if(res.userSub){
-         if(await checkUser(res.userSub)){
-             form.reset()
+      } catch {
+        setLoginValue(true)
+      }
+    }
+    updateUser()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+
+  const signUp = (form) => {
+    Auth.signUp({
+        username: email,
+        password: password,
+        attributes: {
+          email: email,
+          phone_number: phone_number,
+          name: username,
+        }
+      })
+      .then(async (res) => {
+        if (res.userSub) {
+          if (await checkUser(res.userSub)) {
+            form.reset()
             setVerified(true)
             setError(false)
             setLoader(false)
-            }
-        }
-        })
-        .catch((err) => {
-            if(err.message.includes('phone'))
-            {  
-                return(setMessage(renderMessage.phone_Err) , setError(true),setDisable(false)) 
-            }
-            else{
-                return(setMessage(err.message) , setError(true),setDisable(false))
-                
-            }
-    })
-    }
-  
-    const confirmSignUp =() => {
-        Auth.confirmSignUp(email, confirmationCode)
-        .then(() => {
-            Auth.signIn({
-                username: email,
-                password: password
-            })
-            // eslint-disable-next-line no-sequences
-            .then(() =>( history.push('/dashboard'),
-            window.location.reload() 
-            ))
-            .catch((err) => console.log(err))
-        })
-        .catch((err) => setCodeError(true))
-    }
- 
-    function validateEmail(email) {
-        // eslint-disable-next-line no-useless-escape
-        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(String(email).toLowerCase());
-    }
-    const ValidateCode= ()=> {
-        if(!confirmationCode){
-            setEmptyCodeError(true)
-        }
-        else{
-            return true
-        }
-    }
-
-    const Validation = () => {
-        if(!username){
-            setFirstNameError(true)
-        }
-        if(username){
-            if(username.length<=3){
-            setFirstError(true)
-            }
-        }
-        if(!loc){
-            setLocError(true)
-        }
-        if(loc){
-        if(!name){
-            setError(true)
-            setMessage(renderMessage.Err)
-
-        } else if(!businessInfo){
-            setError(true)
-            setMessage(renderMessage.Err)
-
-        }
-    }
-        if(!phone_number){
-            setPhoneError(true)
-        }
-        if(phone_number){
-            if(phone_number.length<=5){
-              setPhoneShort(true)
-            }
-            else if(phone_number.length>=50){
-                setPhoneLong(true)
-            }
-        }
-        if(!validateEmail(email)){
-            setEmailError(true)
-        }
-        if(!password){
-            setPasswordError(true)
-        }
-        if(password){
-        if(password.length<=7){
-            setError(true)
-            setMessage(renderMessage.pass_length)
-    }
-}
-        if(username && loc && username.length>3 && phone_number && validateEmail(email) && password && name && password.length>7 && phone_number.length>=5 &&
-        phone_number.length<=50 ){
-            return true
-        }
-
-    }
-  
-   const handleSubmit = async (e) => {
-        e.preventDefault();
-        const form = e.target
-        setMessage()
-        if (verified && ValidateCode()) {
-            setMessage()
-            confirmSignUp();
-            setLoader(true)
           }
-        if(!verified && Validation()){
-            setLoader(true)
-            setDisable(true)
-            if(await checkBusiness()){
-           setMessage()
-           setError(false)
-           signUp(form)
-            }
-            else{
-                setDisable(false)
-                setError(true)
-                setMessage(renderMessage.Busi_Err)
-
-            }
-        
-    }
-}
-
-    const checkBusiness = async() => {
-        
-        const val = await callApi(name)
-        if(val.length!==0 && val[0].userSub){
-           return false
+        }
+      })
+      .catch((err) => {
+        if (err.message.includes('phone')) {
+          return (setMessage(renderMessage.phone_Err), setError(true), setDisable(false))
+        } else {
+          return (setMessage(err.message), setError(true), setDisable(false))
 
         }
-        else{
-            return true
-        }
-        
-}
-  const checkUser = async(userSub) => {
-     const val = await callApi(name)
-     if(val.length === 0){
-     await addBusiness(userSub,businessInfo)
-     return true
+      })
   }
-  else if(val.length!==0 && !val[0].userSub){
-     await updateBusiness(val[0],userSub)
-     return true
-}
- }
-    const handleChange = (e) => {
-        setFirstNameError(false)
-        setFirstError(false)
-        setPhoneError(false)
-        setEmailError(false)
-        setPasswordError(false)
-        setError(false)
-        setCodeError(false)
-        setLocError(false)
-        setLoader(false)
-        setEmptyCodeError(false)
-        setPhoneLong(false)
-        setPhoneShort(false)
-        if (e.target.id === 'username') {
-            setUser(e.target.value)
-        }
-         else if (e.target.id === 'password') {
-          setPassword(e.target.value)
-        } else if (e.target.id === 'phone_number') {
-          setPhoneNumber(e.target.value)
-        } else if (e.target.id === 'email') {
-          setEmail(e.target.value.trim())
-        } else if (e.target.id === 'confirmationCode') {
-          setconfirmationCode(e.target.value)
-        }
-        else if (e.target.id === 'location') {
-            setName('')
-            setBusinessInfo('')
-            setLoc(e.target.value)
-          }
+
+  const confirmSignUp = () => {
+    Auth.confirmSignUp(email, confirmationCode)
+      .then(() => {
+        Auth.signIn({
+            username: email,
+            password: password
+          })
+          // eslint-disable-next-line no-sequences
+          .then(() => (history.push('/dashboard'),
+            window.location.reload()
+          ))
+          .catch((err) => console.log(err))
+      })
+      .catch((err) => setCodeError(true))
+  }
+
+  function validateEmail(email) {
+    // eslint-disable-next-line no-useless-escape
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+  const ValidateCode = () => {
+    if (!confirmationCode) {
+      setEmptyCodeError(true)
+    } else {
+      return true
+    }
+  }
+
+  const Validation = () => {
+    if (!username) {
+      setFirstNameError(true)
+    }
+    if (username) {
+      if (username.length <= 3) {
+        setFirstError(true)
+      }
+    }
+    if (!loc) {
+      setLocError(true)
+    }
+    if (loc) {
+      if (!name) {
+        setError(true)
+        setMessage(renderMessage.Err)
+
+      } else if (!businessInfo) {
+        setError(true)
+        setMessage(renderMessage.Err)
+
+      }
+    }
+    if (!phone_number) {
+      setPhoneError(true)
+    }
+    if (phone_number) {
+      if (phone_number.length <= 5) {
+        setPhoneShort(true)
+      } else if (phone_number.length >= 50) {
+        setPhoneLong(true)
+      }
+    }
+    if (!validateEmail(email)) {
+      setEmailError(true)
+    }
+    if (!password) {
+      setPasswordError(true)
+    }
+    if (password) {
+      if (password.length <= 7) {
+        setError(true)
+        setMessage(renderMessage.pass_length)
+      }
+    }
+    if (username && loc && username.length > 3 && phone_number && validateEmail(email) && password && name && password.length > 7 && phone_number.length >= 5 &&
+      phone_number.length <= 50) {
+      return true
     }
 
-    return(
-        <>{loginValue=== true?
-        <Wrapper type ={type} page='register' welcomeMessage={renderMessage.New_Reg}>
-            <RegisterForm
-                  type ={type}
-                  err={err}
-                  firstError={firstError}
-                  passwordError={passwordError}
-                  loader={loader}
-                  message={message}
-                  handleChange={handleChange}
-                  handleSubmit={handleSubmit}
-                  verified={verified}
-                  business={business}
-                  setbusiness={setbusiness}
-                  setBusinessInfo={setBusinessInfo}
-                  setName={setName}
-                  codeError={codeError}
-                  firstNameError={firstNameError}
-                  phoneError={phoneError}
-                  emailError={emailError}
-                  locationError={locationError}
-                  emptyCode={emptyCode}
-                  phoneLong={phoneLong}
-                  phoneShort={phoneShort}
-                  password={password}
-                  disable={disable}
-                  />
-        </Wrapper>:<div style={{textAlign:'center' ,margin:' 40px auto 0'}}><ValueLoader height="100" width="100" /></div>
-        }</>
-   
-    )
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const form = e.target
+    setMessage()
+    if (verified && ValidateCode()) {
+      setMessage()
+      confirmSignUp();
+      setLoader(true)
+    }
+    if (!verified && Validation()) {
+      setLoader(true)
+      setDisable(true)
+      if (await checkBusiness()) {
+        setMessage()
+        setError(false)
+        signUp(form)
+      } else {
+        setDisable(false)
+        setError(true)
+        setMessage(renderMessage.Busi_Err)
+
+      }
+
+    }
+  }
+
+  const checkBusiness = async () => {
+
+    const val = await callApi(name)
+    if (val.length !== 0 && val[0].userSub) {
+      return false
+
+    } else {
+      return true
+    }
+
+  }
+  const checkUser = async (userSub) => {
+    const val = await callApi(name)
+    if (val.length === 0) {
+      await addBusiness(userSub, businessInfo)
+      return true
+    } else if (val.length !== 0 && !val[0].userSub) {
+      await updateBusiness(val[0], userSub)
+      return true
+    }
+  }
+
+  const handleChange = (e) => {
+    setFirstNameError(false)
+    setFirstError(false)
+    setPhoneError(false)
+    setEmailError(false)
+    setPasswordError(false)
+    setError(false)
+    setCodeError(false)
+    setLocError(false)
+    setLoader(false)
+    setEmptyCodeError(false)
+    setPhoneLong(false)
+    setPhoneShort(false)
+    if (e.target.id === 'username') {
+      setUser(e.target.value)
+    } else if (e.target.id === 'password') {
+      setPassword(e.target.value)
+    } else if (e.target.id === 'phone_number') {
+      setPhoneNumber(e.target.value)
+    } else if (e.target.id === 'email') {
+      setEmail(e.target.value.trim())
+    } else if (e.target.id === 'confirmationCode') {
+      setconfirmationCode(e.target.value)
+    } else if (e.target.id === 'location') {
+      setName('')
+      setBusinessInfo('')
+      setLoc(e.target.value)
+    }
+  }
+
+  return ({
+      loginValue === true ?
+      <
+      Wrapper type = {
+        type
+      }
+      page = 'register'
+      welcomeMessage = {
+        renderMessage.New_Reg
+      } >
+      <
+      RegisterForm
+      type = {
+        type
+      }
+      err = {
+        err
+      }
+      firstError = {
+        firstError
+      }
+      passwordError = {
+        passwordError
+      }
+      loader = {
+        loader
+      }
+      message = {
+        message
+      }
+      handleChange = {
+        handleChange
+      }
+      handleSubmit = {
+        handleSubmit
+      }
+      verified = {
+        verified
+      }
+      business = {
+        business
+      }
+      setbusiness = {
+        setbusiness
+      }
+      setBusinessInfo = {
+        setBusinessInfo
+      }
+      setName = {
+        setName
+      }
+      codeError = {
+        codeError
+      }
+      firstNameError = {
+        firstNameError
+      }
+      phoneError = {
+        phoneError
+      }
+      emailError = {
+        emailError
+      }
+      locationError = {
+        locationError
+      }
+      emptyCode = {
+        emptyCode
+      }
+      phoneLong = {
+        phoneLong
+      }
+      phoneShort = {
+        phoneShort
+      }
+      password = {
+        password
+      }
+      disable = {
+        disable
+      }
+      /> < /
+      Wrapper > : < div style = {
+        {
+          textAlign: 'center',
+          margin: ' 40px auto 0'
+        }
+      } > < ValueLoader height = "100"
+      width = "100" / > < /div>
+    } < />
+
+  )
 
 
 }
