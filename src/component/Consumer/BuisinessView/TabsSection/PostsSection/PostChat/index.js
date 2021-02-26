8f88d91953eed0e9bf167876fa613f83ebd64906
Shivika <shivika.singla@vtnetzwelt.com>
2021-02-26 16:46:33 +0530

    Infinite Scroller in posts

--- src/component/Consumer/BuisinessView/TabsSection/PostsSection/PostChat/index.js
+++ src/component/Consumer/BuisinessView/TabsSection/PostsSection/PostChat/index.js
@@ -1,40 +1,69 @@
-import React from "react";
-import styled from "styled-components"
-import UserMessage from './UserMessage'
-import { Scrollbars } from 'react-custom-scrollbars';
+import React, { useState } from "react";
+import styled from "styled-components";
+import UserMessage from "./UserMessage";
+import { Scrollbars } from "react-custom-scrollbars";
 import { useSelector } from "react-redux";
-
+import InfiniteScroll from "react-infinite-scroll-component";
+import ValueLoader from "../../../../../../utils/loader";
 
 const ChatContent = styled.div`
-    width:100%;
-    position: relative;
-    display:flex;
-    padding: 12px 0;
-    flex-direction: column;
-    overflow: hidden;
-    @media (max-width:767px){
-    }
-`
-
+  width: 100%;
+  position: relative;
+  display: flex;
+  padding: 12px 0;
+  flex-direction: column;
+  overflow: hidden;
+  @media (max-width: 767px) {
+  }
+`;
 
 const PostChat = ({}) => {
-    const posts = useSelector((state) => state.business.posts);
-    return (
+  const posts = useSelector((state) => state.business.posts);
+  const [eventPosition, setEventPosition] = useState(20);
+  const fetchMorePosts = () => {
+    setTimeout(() => {
+      setEventPosition(eventPosition + 20);
+    }, 4000);
+  };
+  return (
     <>
-     <Scrollbars  
+      <div id="scrollableDiv">
+        <Scrollbars
         autoHeight
         autoHeightMin={0}
         autoHeightMax={450}
         thumbMinSize={30}
+      >
+        <InfiniteScroll
+          dataLength={posts.length}
+          next={fetchMorePosts}
+          hasMore={true}
+          loader={
+            posts && eventPosition < posts.length ? (
+              <div style={{ textAlign: "center", margin: " 40px auto 0" }}>
+                {" "}
+                <ValueLoader height="40" width="40" />
+              </div>
+            ) : (
+              <center>
+                <p>No more posts to show</p>
+              </center>
+            )
+          }
+          scrollableTarget="scrollableDiv"
         >
-        <ChatContent>
-        {posts.map((i) => (
-            <UserMessage postData={i} />
-          ))}
-        </ChatContent>
-    </Scrollbars>
+          <ChatContent>
+            {posts
+              ? posts
+                  .slice(0, eventPosition)
+                  .map((i) => <UserMessage postData={i} />)
+              : null}
+          </ChatContent>
+        </InfiniteScroll>
+        </Scrollbars>
+      </div>
     </>
-    )
-}
-  
-  export default PostChat
+  );
+};
+
+export default PostChat;
