84e147abc89b5f8a7cb15097941988f0cd008bd2
Shivika <shivika.singla@vtnetzwelt.com>
2021-03-17 12:22:52 +0530

    mentions UI issue fixed in posts

--- src/component/Consumer/BuisinessView/TabsSection/PostsSection/PostChat/UserMessage/ScrollToBottom1.js
+++ src/component/Consumer/BuisinessView/TabsSection/PostsSection/PostChat/UserMessage/ScrollToBottom1.js
@@ -3,7 +3,7 @@ import React, { useRef, useEffect } from "react";
 const ScrollToBottom1 = () => {
   const elementRef1 = useRef(null);
   useEffect(() =>
-    elementRef1.current.scrollIntoView({ behavior: "smooth", block: "end" })
+    elementRef1.current.scrollIntoView({ behavior: "smooth", block: "center" })
   );
   return <div ref={elementRef1} />;
 };
