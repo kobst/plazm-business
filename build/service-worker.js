4d22c6fb0b163ffbff51464724cd1c8ce1db6cc1
Fahad Sadiq <fahad.sadiq@vtnetzwelt.com>
2021-05-19 11:26:50 +0530

    Build for 9dddb6ef45dd51af380c1c8b6de9f9db56509303

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9e191d77808de39f095d60ec91ad3e95.js"
+  "/precache-manifest.616dfc1a527aebdae94c1faf98992d9c.js"
 );
 
 self.addEventListener('message', (event) => {
