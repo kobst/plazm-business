c115fc4349d0db648152725732dce760aa0e2c2d
kobst <edwrdhn@gmail.com>
2021-08-13 19:21:53 -0400

    testing commit

--- src/component/Consumer/AddPostModal/index.js
+++ src/component/Consumer/AddPostModal/index.js
@@ -5,8 +5,6 @@ import AllListingsContent from "../AddPostModal/AllListings";
 import CreateListModel from "./createList";
 
 
-
-
 const ModalContent = styled.div`
   width: 100%;
   position: relative;
