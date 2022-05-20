7db9b6254510affba1ff438e538cd61273473a0f
Baljeet Singh <baljeet.singh@vtnetzwelt.com>
2022-05-20 13:51:38 +0530

    MCBM-793 New List Modal Functionality

--- src/component/Consumer/AddPostModal/createList/ImageHandler/index.js
+++ src/component/Consumer/AddPostModal/createList/ImageHandler/index.js
@@ -1,41 +1,17 @@
-import React, { useCallback, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import { useDropzone } from "react-dropzone";
-import ReactCrop, {centerCrop, makeAspectCrop,} from 'react-image-crop'
+import ReactCrop from 'react-image-crop'
 import 'react-image-crop/dist/ReactCrop.css'
 import { FiPlus } from "react-icons/fi";
-import { ContentTabPanel, PlusIcon, ClickText, ClickTextBottom, CroppedFinalImgSec, FinalImgdesp } from '../styles'
-import { dataUrlToFile, getCroppedImg } from "../../../../../utils/image";
-// import { imgPreview } from './ImgPreview'
-
-function centerAspectCrop(
-  mediaWidth,
-  mediaHeight,
-  aspect,
-) {
-  return centerCrop(
-    makeAspectCrop(
-      {
-        unit: '%',
-        width: 90,
-      },
-      aspect,
-      mediaWidth,
-      mediaHeight,
-    ),
-    mediaWidth,
-    mediaHeight,
-  )
-}
+import { ContentTabPanel, PlusIcon, ClickText, ClickTextBottom } from '../styles'
+import { dataUrlToFile, cropImageNow } from "../../../../../utils/image";
 
 const ImageHandler = ({croppedImage, setCroppedImage, imagePreview, setImagePreview}) => {
   const [imgSrc, setImgSrc] = useState();
   const [imageFile, setImageFile] = useState();
-  // const [croppedImage, setCroppedImage] = useState();
-  const previewCanvasRef = useRef(null)
   const imgRef = useRef(null)
-  const [crop, setCrop] = useState({unit: '%'});
+  const [crop, setCrop] = useState();
   const [completedCrop, setCompletedCrop] = useState();
-  const [aspect, setAspect] = useState()
   const { getRootProps, getInputProps } = useDropzone({
     accept:{
       'image/*': ['.jpeg', '.jpg', '.png']
@@ -53,30 +29,12 @@ const ImageHandler = ({croppedImage, setCroppedImage, imagePreview, setImagePrev
   });
 
   const showCroppedImage = async () => {
-    const { width, height } = imgRef.current
-    console.log(imgRef.current, 'imgRef.current', width, height);
     try {
-      console.log(imgSrc);
-      const img = await getCroppedImg(
-        imgSrc,
-        completedCrop,
-        0,
-        imageFile.type
+      const img = await cropImageNow(
+        imgRef.current,
+        crop
       )
-      // console.log('donee', { img })
-      // setImagePreview(img)
-      // const ip = await imgPreview(imgRef.current, crop)
       setImagePreview(img)
-      // console.log('===================ip=================');
-      // console.log(ip);
-      // console.log('====================================');
-      // canvasPreview(
-      //   imgRef.current,
-      //   previewCanvasRef.current,
-      //   completedCrop,
-      //   undefined,
-      //   1,
-      // )
       setCompletedCrop(null)
       setImgSrc(null)
       const file = await dataUrlToFile(img, imageFile.name, imageFile.type)
@@ -85,13 +43,6 @@ const ImageHandler = ({croppedImage, setCroppedImage, imagePreview, setImagePrev
       console.error(e)
     }
 
-  }
-
-  function onImageLoad(e) {
-    if (aspect) {
-      const { width, height } = e.currentTarget
-      setCrop(centerAspectCrop(width, height, aspect))
-    }
   }
 
   const cropAgain = () => {
@@ -114,23 +65,14 @@ const ImageHandler = ({croppedImage, setCroppedImage, imagePreview, setImagePrev
         </ClickTextBottom>
       </ContentTabPanel>)}
       {!imagePreview && <ContentTabPanel>
-        <ReactCrop  aspect={aspect} crop={crop} onComplete={(p) => setCompletedCrop(p)} onChange={(_, percentCrop) => setCrop(percentCrop)}>
-          <img ref={imgRef} src={imgSrc} onLoad={onImageLoad} />
+        <ReactCrop crop={crop} onComplete={(p) => setCompletedCrop(p)} onChange={setCrop}>
+          <img ref={imgRef} src={imgSrc} />
         </ReactCrop>
       </ContentTabPanel>}
       {completedCrop && <button onClick={() => showCroppedImage()}>Confirm</button>}
       {imagePreview &&
       <>
           <img src={imagePreview} />
-          {/* <canvas
-            ref={previewCanvasRef}
-            style={{
-              border: '1px solid black',
-              objectFit: 'contain',
-              width: completedCrop.width,
-              height: completedCrop.height,
-            }}
-          /> */}
           <button onClick={() => cropAgain()}>Crop Again</button>
         </>
       }
