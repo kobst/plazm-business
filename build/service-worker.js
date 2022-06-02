d55bb247b44f6a7dcfe235014e2ae6263180d827
Fahad Sadiq <fahad@netaesthetics.com>
2022-06-02 11:26:08 +0530

    Build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0457487ac790207e23b1ebe77e4a5dd4.js"
+  "/precache-manifest.84bbd71c634bf9a77883592a9ac7879e.js"
 );
 
 self.addEventListener('message', (event) => {
