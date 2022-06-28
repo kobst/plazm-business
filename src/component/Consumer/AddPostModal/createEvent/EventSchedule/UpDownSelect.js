7032ea6f2bcc13505e75aaeb51ec9f3363dc45d4
Baljeet Singh <baljeet.singh@vtnetzwelt.com>
2022-06-28 17:41:50 +0530

    mcbm-909: event calendar must be well align and also as per design

--- src/component/Consumer/AddPostModal/createEvent/EventSchedule/UpDownSelect.js
+++ src/component/Consumer/AddPostModal/createEvent/EventSchedule/UpDownSelect.js
@@ -1,28 +1,13 @@
-import React, { useState, useRef, useEffect } from "react";
+import React from "react";
 import {
   DateText,
   DateDropdown,
-  DropDownSection,
 } from "../styled.js";
 import { IoMdArrowDropdown, IoMdArrowDropup } from "react-icons/io";
 
 
 const UpDownSelect = ({options, value, onChange}) => {
-  const [open, setOpen] = useState();
-  const ref = useRef();
-
-  const handleClickOutside = (e) => {
-    if (!ref.current.contains(e.target) && open) {
-      setOpen(false);
-    }
-  };
-
-  const onSelect = (v) => {
-    setOpen(false);
-    onChange(v)
-  }
-
-  const onUp = () => {
+   const onUp = () => {
     const index = options.indexOf(value)
     if(index > 0) {
       onChange(options[index-1])
@@ -37,24 +22,12 @@ const UpDownSelect = ({options, value, onChange}) => {
     }
   }
 
-  // useEffect(() => {
-  //   document.addEventListener("mousedown", handleClickOutside);
-  //   return () => document.removeEventListener("mousedown", handleClickOutside);
-  // });
-
   return (
-    <DateText ref={ref}>
+    <DateText>
       {value ?? '--'}
       <DateDropdown>
         <IoMdArrowDropup onClick={() => onUp()} className="DropupIcon" /> <IoMdArrowDropdown onClick={() => onDown()} />
       </DateDropdown>
-      {/* <DropDownSection isOpen={open}>
-        <ul>
-          {options.map((option) => (
-            <li key={option} onClick={() => onSelect(option)}>{option}</li>
-          ))}
-        </ul>
-      </DropDownSection> */}
     </DateText>
   );
 };
