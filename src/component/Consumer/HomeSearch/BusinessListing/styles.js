551d4c9f02cc729893a5cbb1bc5879588cd9d054
pankaj sehgal <pankaj.sehgal@vtnetzwelt.com>
2022-12-12 17:02:07 +0530

    mcbm-958 Styling In progress

---
+++ src/component/Consumer/HomeSearch/BusinessListing/styles.js
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+export const SearchListingContent = styled.ul`
+  display: flex;
+  background-color: #f3f3f3;
+  align-items: flex-start;
+  justify-items: center;
+  flex-direction: column;
+  overflow: auto;
+  overflow-x: hidden;
+  padding: 0;
+  margin: 0 10px;
+  max-height: 200px;
+  ::-webkit-scrollbar {
+    width: 6px;
+    height: 5px;
+    cursor: pointer;
+  }
+  ::-webkit-scrollbar-track {
+    border-radius: 0px;
+  }
+  ::-webkit-scrollbar-thumb {
+    background: #000;
+    border-radius: 0px;
+    cursor: pointer;
+    width: 6px;
+    height: 5px;
+  }
+  ::-webkit-scrollbar-thumb:hover {
+    background: #000;
+  }
+  li {
+    width: 100%;
+    border-bottom: 1px solid #ccc;
+    padding: 5px 10px;
+    cursor: pointer;
+    font-size: 13px;
+    color: #333;
+    font-weight: 500;
+    &:hover {
+      color: #000;
+      font-weight: 600;
+    }
+  }
+`;
